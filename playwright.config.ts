f6487a9153d3b8c77d6b0101a61ed1fd14d3f30b
Clement <mazeaucleemnt03@gmail.com>
2026-02-12 17:11:54 +0100

    modif config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from '@playwright/test';
 import dotenv from 'dotenv';
 import path from 'path';
 
+
+dotenv.config({ path: path.resolve(__dirname, 'env/.env') });
+
 export default defineConfig({
   testDir: './tests',
   use: {
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure'
+    video: 'retain-on-failure',
   },
 });
-
-dotenv.config({path: path.resolve(__dirname, 'env/.env')});
